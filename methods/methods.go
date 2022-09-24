0810b2ae9f53ec80b58dc3cd5129ba0ef512294f
Saki Nakazato <snkzt@mercari.com>
2022-09-24 16:20:34 +0900

    [Methods] Add methods.go

---
+++ methods/methods.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type rectangle struct {
+	width, height int
+}
+
+// Pointer type method
+// Use this type to avoid copying on method calls or
+// to allow the method to mutate the receiving struct
+func (r *rectangle) area() int {
+	return r.width * r.height
+}
+
+// Value receiver type method
+func (r rectangle) perim() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+	r := rectangle{width: 10, height: 5}
+	fmt.Println("area:", r.area())   // 50 mutating the struct
+	fmt.Println("perim:", r.perim()) // 30 initialised a new struct
+
+	rp := &r
+	fmt.Println("area:", rp.area())   // 50
+	fmt.Println("perim:", rp.perim()) // 30
+
+	r = rectangle{height: 5}
+	fmt.Println("area:", r.area())                     // 0 mutating the struct
+	fmt.Println("Struct is now width:0, height:5", rp) // Because rectangle is dereferenced and mutated above
+
+}
