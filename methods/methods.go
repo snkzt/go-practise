bb5b30dc7d04f37ade540b19a29fe11abb8982e5
Saki Nakazato <snkzt@mercari.com>
2022-09-24 17:22:50 +0900

    [Interface] Add interfaces.go

--- methods/methods.go
+++ methods/methods.go
@@ -8,6 +8,9 @@ type rectangle struct {
 	width, height int
 }
 
+// A method is a function with an extra receiver argument
+// The receiver sits between the func keyword and the method name.
+
 // Pointer type method
 // Use this type to avoid copying on method calls or
 // to allow the method to mutate the receiving struct
@@ -32,5 +35,4 @@ func main() {
 	r = rectangle{height: 5}
 	fmt.Println("area:", r.area())                     // 0 mutating the struct
 	fmt.Println("Struct is now width:0, height:5", rp) // Because rectangle is dereferenced and mutated above
-
 }
